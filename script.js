99ee3bc48533813c17db56cce54e832a1d472429
devmonca2 <devmonca@gmail.com>
2025-02-19 15:57:07 -0300

    Project created

---
+++ script.js
@@ -0,0 +1,3 @@
+const inputName = document.getElementById("input-name");
+const inputEmail = document.getElementById("input-email");
+const inputPassword = document.getElementById("input-password");
